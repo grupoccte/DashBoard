2952634d4fd24d8bf6f09d6dfe3496fc2f554664
Pedro Neto <Pedro Neto>
2016-09-02 09:21:25 -0300

    Add estrutura visual

--- server.R
+++ server.R
@@ -1,7 +1,26 @@
 library(shiny)
+library(shinydashboard)
 
 shinyServer(function(input, output) {
-
+  #Radio da aplicação
+  output$radioApp <- renderUI({
+    radioButtons("aplicacao","Seleciona a aplicação:", 
+                 choices =c("Visão geral dos dados" = "geral",
+                            "Análise de Desempenho" = "analiseDesempenho",
+                            "Análise de Evasão" = "analiseEvasao"),selected = "geral")
+  })
   
+  #Select para Curso
+  output$seletorCurso <- renderUI({
+    selectInput("curso", "Escolha o Curso:", choices = (""))
+  })
+  #Select para o periodo a depender do curso escolhido
+  output$seletorPeriodo <- renderUI({
+    selectInput("periodo", "Escolha o Periodo:", choices = (""))
+  })
+  #Select da disciplina a depender do curso e do periodo escolhido
+  output$seletorDisciplina <- renderUI({
+    selectInput("disciplina","Escolha a Disciplina:",choices = (""))
+  })
 
 })
--- ui.R
+++ ui.R
@@ -1,3 +1,38 @@
 library(shiny)
+library(shinydashboard)
 
-shinyUI()
+dashboardPage(
+  dashboardHeader(title = "Dashboard"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Parametros",tabName = "Parametros", icon = icon("dashboard"))
+    ),
+    uiOutput("radioApp"),
+    uiOutput("seletorCurso"),
+    uiOutput("seletorPeriodo"),
+    uiOutput("seletorDisciplina")
+    ),
+  dashboardBody(
+    fluidRow(
+      column(
+        width=8,
+        tabBox(
+          side = "left", width = 12,
+          selected = "Geral",
+          tabPanel("Geral", ""),
+          tabPanel("Indicadores", ""),
+          tabPanel("Alunos", "")
+        )
+      ),
+      column(
+        width = 4,
+        box(
+          width = 12, title = "Indicadores",status = "primary",solidHeader = TRUE ,collapsible = TRUE
+        ),
+        box(
+          width=12,title = "Alunos",status = "primary",solidHeader = TRUE ,collapsible = TRUE
+        )
+      )
+    )
+  )
+)
